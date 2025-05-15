7acdd1560f128cc19902bdd3f9f9f3c7186be443
Uday Gadekar <gadekarraviraj16@gmail.com>
2025-05-15 21:03:20 +0530

    reverse sll.c
    "Implemented a function to reverse a Singly Linked List by rearranging node links, effectively reversing the order of elements in the list."

---
+++ reverse sll.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+struct Node* reverseList(struct Node* head) {
+    struct Node *prev = NULL, *curr = head, *next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+int main() {
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+
+    printf("Original List: ");
+    printList(head);
+
+    head = reverseList(head);
+
+    printf("Reversed List: ");
+    printList(head);
+
+    return 0;
+}
